71964f59ce8d6704f4b510b842f6155046ad8213
atitorenko <a.a.titorenko@gmail.com>
2017-10-15 12:12:57 +0300

    Lab1

--- BrowserStatistics/src/main/java/Driver.java
+++ BrowserStatistics/src/main/java/Driver.java
@@ -1,11 +1,15 @@
 package main.java;
 
+import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Counter;
+import org.apache.hadoop.mapreduce.Counters;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.LazyOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 
@@ -17,19 +21,27 @@ public class Driver extends Configured implements Tool {
             System.out.println("Two params are required- <input path> <output path>");
         }
         Job job = Job.getInstance(getConf());
+
         job.setJobName("Browser statistics");
         job.setJarByClass(Driver.class);
-        job.setInputFormatClass(NLineInputFormat.class);
-        NLineInputFormat.addInputPath(job, new Path(args[0]));
-        job.getConfiguration().setInt("mapreduce.input.lineinputformat.setlinespermap", 5);
-        ParagraphR
+
+        job.setInputFormatClass(BSInputFormat.class);
+        BSInputFormat.addInputPath(job, new Path(args[0]));
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(LongWritable.class);
         LazyOutputFormat.setOutputFormatClass(job, TextOutputFormat.class);
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        getConf().set("mapreduce.output.textoutputformat.separator", ",");
 
-        job.setMapperClass(MapperNLogs.class);
-        job.setNumReduceTasks(0); //TODO: what is it?
+        job.setMapperClass(BSMapper.class);
+        job.setReducerClass(BSReducer.class);
+
+        //    job.setCombinerClass(IntSumReducer.class); TODO: делать или нет?
 
         boolean success = job.waitForCompletion(true);
+        Counter counter = job.getCounters().findCounter(BS_COUNTER.MALFORMED_ROWS); //TODO: куда-то запихнуть счетчики.
+        System.out.println("Malformed rows count:" + counter.getValue());
         return  success ? 1 : 0;
     }
 
